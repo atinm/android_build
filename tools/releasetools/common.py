e90dd1047c16fbd5de21b644493e3a36664768e4
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-04-30 07:02:50 -0700

    die python die!

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -147,10 +147,10 @@ def LoadRecoveryFSTab(zip):
   try:
     data = zip.read("RECOVERY/RAMDISK/etc/recovery.fstab")
   except KeyError:
-  try:
-    data = zip.read("RECOVERY/RAMDISK/misc/recovery.fstab")
-  except KeyError:
-    raise ValueError("Could not find RECOVERY/RAMDISK/{etc,misc}/recovery.fstab")
+    try:
+      data = zip.read("RECOVERY/RAMDISK/misc/recovery.fstab")
+    except KeyError:
+      raise ValueError("Could not find RECOVERY/RAMDISK/{etc,misc}/recovery.fstab")
 
   d = {}
   for line in data.split("\n"):
